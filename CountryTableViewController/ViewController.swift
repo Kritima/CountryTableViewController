50a1894ad85064683bfe0ef03f24343edeaece81
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-03-04 21:07:32 -0500

    Initial Commit

--- CountryTableViewController/ViewController.swift
+++ CountryTableViewController/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tblCountry: UITableView!
+    
+    lazy var countryNames = ["Afghanistan","Albania","Algeria","Bahrain","Canada"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadCountries()
         // Do any additional setup after loading the view.
+        
     }
+    
+    func loadCountries()
+    {
+        countryNames.append(Country(name:"Afghanistan", capital: "Kabul", flag: #imageLiteral(resourceName: <#T##String#>)))
+        countryNames.append(Country(name:"Albania", capital: "Kabul", flag: #imageLiteral(resourceName: <#T##String#>)))
+        countryNames.append(Country(name:"Algeria", capital: "Kabul", flag: #imageLiteral(resourceName: <#T##String#>)))
+        countryNames.append(Country(name:"Algeria", capital: "Kabul", flag: #imageLiteral(resourceName: <#T##String#>)))
 
-
+    }
 }
+
+    extension ViewController: UITableViewDataSource, UITableViewDelegate
+    {
+        func numberOfSections(in tableView: UITableView) -> Int {
+               // #warning Incomplete implementation, return the number of sections
+               return 1
+           }
+           
+           func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+               // #warning Incomplete implementation, return the number of rows
+               return countryNames.count
+           }
+           
+           func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+               let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
+               
+               let country = countryNames[indexPath.row]
+            cell?.textLabel?.text = country.name
+               cell?.detailTextLabel?.text = country.capital
+            cell?.imageView?.text = country.capital.flag
+               return cell
+        
+        
+    }
+}
+
+
 
